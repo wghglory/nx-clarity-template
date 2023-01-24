39edcf67e9670837cf0c0aa8ede30c1811748e02
Derek Wang <guanghuiw@vmware.com>
2023-01-25 00:07:51 +0800

    feat: add clarity

--- apps/seed/src/app/app.module.ts
+++ apps/seed/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClarityModule } from '@clr/angular';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule, ClarityModule],
   providers: [],
   bootstrap: [AppComponent],
 })
