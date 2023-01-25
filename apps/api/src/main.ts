57a8a02aeb0abaa9f179d6fd418417d39665680c
Derek Wang <guanghuiw@vmware.com>
2023-01-25 08:41:43 +0800

    refactor: mock rename to api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to mock!' });
+  res.send({ message: 'Welcome to api!' });
 });
 
 const port = process.env.PORT || 3333;
