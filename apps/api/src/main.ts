3214f990d4147b02570bdc1cc2f5a6e353a1d702
Derek Wang <guanghuiw@vmware.com>
2023-01-25 12:18:27 +0800

    refactor: api express server

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,7 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import express from 'express';
 import * as path from 'path';
+
+import { productRouter } from './routes/product.route';
 
 const app = express();
 
@@ -14,24 +11,7 @@ app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to api!' });
 });
 
-const products = [
-  { id: 1, name: 'Basketball' },
-  { id: 2, name: 'Football' },
-];
-
-app.get('/api/products', (req, res) => {
-  res.send(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const id = +req.params.id;
-  const found = products.find((p) => p.id === id);
-
-  if (found) {
-    return res.send(found);
-  }
-  return res.status(404).send({ message: 'not found' });
-});
+app.use(`/api/products`, productRouter);
 
 const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
