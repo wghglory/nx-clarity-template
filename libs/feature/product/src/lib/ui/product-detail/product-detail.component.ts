d46a4ee23448b946908d0bc033ca192427c5304e
Derek Wang <guanghuiw@vmware.com>
2023-01-26 09:26:38 +0800

    feat: loading or error for product detail

--- libs/feature/product/src/lib/ui/product-detail/product-detail.component.ts
+++ libs/feature/product/src/lib/ui/product-detail/product-detail.component.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs';
+import { LoadingOrErrorComponent } from '@seed/shared/ui';
+import { catchError, EMPTY, Subject, switchMap } from 'rxjs';
 
 import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'seed-product-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LoadingOrErrorComponent],
   templateUrl: './product-detail.component.html',
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -19,9 +21,15 @@ export class ProductDetailComponent {
     private route: ActivatedRoute
   ) {}
 
+  readonly error$ = new Subject<HttpErrorResponse>();
+
   product$ = this.route.params.pipe(
     switchMap(({ id }) => {
       return this.productService.getProduct(id);
+    }),
+    catchError((err) => {
+      this.error$.next(err);
+      return EMPTY;
     })
   );
 }
