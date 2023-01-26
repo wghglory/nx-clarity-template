018321b944e7fa46ca22f6a57e2e5cd069f27d0c
Derek Wang <guanghuiw@vmware.com>
2023-01-26 14:59:27 +0800

    feat: component store

--- libs/feature/product/src/lib/ui/product-list/product-list.component.ts
+++ libs/feature/product/src/lib/ui/product-list/product-list.component.ts
@@ -1,30 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { HttpErrorResponse } from '@angular/common/http';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ClarityModule } from '@clr/angular';
 import { LoadingOrErrorComponent } from '@seed/shared/ui';
-import { catchError, EMPTY, Subject } from 'rxjs';
 
-import { ProductService } from '../../services/product.service';
+import { ProductListStore } from './product-list.store';
 
 @Component({
   selector: 'seed-product-list',
   standalone: true,
   imports: [CommonModule, RouterModule, ClarityModule, LoadingOrErrorComponent],
   templateUrl: './product-list.component.html',
-  styles: [],
+  providers: [ProductListStore],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductListComponent {
-  constructor(private productService: ProductService) {}
+export class ProductListComponent implements OnInit {
+  constructor(private productListStore: ProductListStore) {}
 
-  error$ = new Subject<HttpErrorResponse>();
+  products$ = this.productListStore.products$;
+  error$ = this.productListStore.error$;
+  loading$ = this.productListStore.loading$;
 
-  products$ = this.productService.products$.pipe(
-    catchError((err) => {
-      this.error$.next(err);
-      return EMPTY;
-    })
-  );
+  vm$ = this.productListStore.vm$;
+
+  ngOnInit() {
+    this.productListStore.getProducts();
+  }
 }
