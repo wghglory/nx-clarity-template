72c6f0ebeba32d810085a1d18198c0881e1ac67a
Derek Wang <guanghuiw@vmware.com>
2023-01-27 16:26:21 +0800

    feat: display product card list

--- libs/feature/product/src/lib/ui/product-card-list/product-card-list.component.ts
+++ libs/feature/product/src/lib/ui/product-card-list/product-card-list.component.ts
@@ -1,12 +1,37 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ClarityModule } from '@clr/angular';
+import { LoadingOrErrorComponent } from '@seed/shared/ui';
+import { catchError, EMPTY, Subject, switchMap } from 'rxjs';
+
+import { ProductService } from '../../services/product.service';
+import { ProductStateService } from '../../services/product-state.service';
 
 @Component({
   selector: 'seed-product-card-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ClarityModule, RouterModule, LoadingOrErrorComponent],
   templateUrl: './product-card-list.component.html',
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductCardListComponent {}
+export class ProductCardListComponent {
+  constructor(
+    private productService: ProductService,
+    private productStateService: ProductStateService
+  ) {}
+
+  error$ = new Subject<HttpErrorResponse>();
+
+  products$ = this.productStateService.refreshAction$.pipe(
+    switchMap(() => {
+      return this.productService.products$;
+    }),
+    catchError((err) => {
+      this.error$.next(err);
+      return EMPTY;
+    })
+  );
+}
