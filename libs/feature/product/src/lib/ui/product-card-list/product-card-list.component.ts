16d0329e7525b35879094561f71964c173a8090d
Derek Wang <guanghuiw@vmware.com>
2023-01-30 16:40:15 +0800

    feat: display load more button and load initial products for card view

--- libs/feature/product/src/lib/ui/product-card-list/product-card-list.component.ts
+++ libs/feature/product/src/lib/ui/product-card-list/product-card-list.component.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { HttpErrorResponse } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ClarityModule } from '@clr/angular';
+import { ClarityModule, ClrDatagridStateInterface } from '@clr/angular';
+import { stateHandler } from '@seed/rde';
 import { LoadingOrErrorComponent } from '@seed/shared/ui';
-import { catchError, EMPTY, Subject, switchMap } from 'rxjs';
+import { BehaviorSubject, catchError, EMPTY, Subject, switchMap } from 'rxjs';
 
 import { Product } from '../../models/product';
 import { ProductService } from '../../services/product.service';
@@ -25,13 +26,23 @@ export class ProductCardListComponent {
 
   error$ = new Subject<HttpErrorResponse>();
 
-  products$ = this.productStateService.refreshAction$.pipe(
-    switchMap(() => {
-      return this.productService.products$;
-    }),
-    catchError((err) => {
-      this.error$.next(err);
-      return EMPTY;
+  // products$ = this.productStateService.refreshAction$.pipe(
+  //   switchMap(() => {
+  //     return this.productService.products$;
+  //   }),
+  //   catchError((err) => {
+  //     this.error$.next(err);
+  //     return EMPTY;
+  //   })
+  // );
+
+  private pageSource = new BehaviorSubject<ClrDatagridStateInterface>({ page: { current: 1, size: 9 } });
+  page$ = this.pageSource.asObservable();
+
+  products$ = this.page$.pipe(
+    switchMap((pageInfo) => {
+      const params = stateHandler(pageInfo);
+      return this.productService.getProducts(params);
     })
   );
 
